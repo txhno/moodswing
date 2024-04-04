e4e9baa6fbfe2ea4a1800135f390b9cd4b724394
roshan <roshwarrier@gmail.com>
2024-04-04 23:06:56 +0530

    increase epochs parameter to 11

--- model/model_training.py
+++ model/model_training.py
@@ -36,7 +36,7 @@ model = Sequential([
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
-model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=11, validation_data=(X_test, y_test)) # Changed epochs from 10 to 11
 
 # Save the model
 model.save('trained_model/emotion_classifier.h5')
